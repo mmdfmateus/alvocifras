4927cc7955def6072e4f81a8e6c4212727d6372c
mmdfmateus <mmdfmateus@gmail.com>
2023-05-29 21:01:27 -0300

    add artists page

--- src/pages/artists/index.tsx
+++ src/pages/artists/index.tsx
@@ -1,9 +1,9 @@
 import { type NextPage } from 'next'
 import Head from 'next/head'
 
+import Image from 'next/image'
 import { Separator } from '~/components/ui/separator'
 import Link from 'next/link'
-import { Dot } from 'lucide-react'
 
 const songs = [
   {
@@ -32,18 +32,18 @@ const Songs: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Músicas - Alvo Cifras</title>
-        <meta name="description" content="Todos as músicas presentes no Alvo Cifras" />
+        <title>Artistas - Alvo Cifras</title>
+        <meta name="description" content="Todos os artistas presentes no Alvo Cifras" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
         <main className="flex w-screen flex-col items-center justify-center">
           <div className="container flex flex-col gap-12 px-8 py-16">
-            <h1 className='text-4xl'>Músicas</h1>
+            <h1 className='text-4xl'>Artistas</h1>
             <ul className='flex flex-col'>
                 {
                   songs.map((song, index) => {
-                    const showInitialLetter = song.song.charAt(0).toLocaleUpperCase() !== currentLetter
-                    currentLetter = song.song.charAt(0).toLocaleUpperCase()
+                    const showInitialLetter = song.artist.charAt(0).toLocaleUpperCase() !== currentLetter
+                    currentLetter = song.artist.charAt(0).toLocaleUpperCase()
                     return (<>
                         { showInitialLetter &&
                           <div>
@@ -56,12 +56,15 @@ const Songs: NextPage = () => {
                           key={index}
                           className="mb-2 h-16 flex items-center p-2 rounded-md hover:bg-slate-100 last:mb-3"
                           >
-                            <Dot size={36} />
-                            <div className="space-y-1">
+                          <Image
+                            src={`${song.imageUrl}.jpg`}
+                            alt='artista'
+                            height={48}
+                            width={48}
+                            className='h-12 w-12 rounded-full mr-4'
+                            />
+                            <div className="space-y-1 flex items-center">
                               <p className="text-sm font-semibold leading-none">
-                                {song.song}
-                              </p>
-                              <p className="text-sm text-muted-foreground">
                                 {song.artist}
                               </p>
                             </div>
