fd1033e0fa5562fef9f765e1b4b81253cc804d44
mmdfmateus <mmdfmateus@gmail.com>
2023-06-20 01:37:14 -0300

    add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import { type AppType } from 'next/app'
 import { type Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
+import { Header } from '~/components/Header'
+import { Toaster } from 'react-hot-toast'
+import { Analytics } from '@vercel/analytics/react';
 
 import { api } from '~/utils/api'
 
 import '~/styles/globals.css'
 import '~/styles/chordsStyles.css'
 import '@uploadthing/react/styles.css'
-import { Header } from '~/components/Header'
-import { Toaster } from 'react-hot-toast'
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -17,6 +18,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <Toaster />
+      <Analytics />
       <Header />
       <Component className='container max-w-screen-lg' {...pageProps} />
     </SessionProvider>
